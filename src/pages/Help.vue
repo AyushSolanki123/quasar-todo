8bf4ad5a090810eb9ec2b4ad8533c4855b21a36c
AyushSolanki123 <ayush.solanki1981@gmail.com>
2022-08-09 19:18:48 +0530

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>This is the Help page of the Todo App</p>
+    <p>Hope your query is Solved</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
